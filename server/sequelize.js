89bf80dfbb7311fd1695da924c5656e1744887ba
ozoq <70523219+ozoq@users.noreply.github.com>
2022-09-27 21:13:39 +0200

    Some copypasta to make sequilize work with Heroku

--- server/sequelize.js
+++ server/sequelize.js
@@ -1,9 +1,43 @@
 const { Sequelize } = require("sequelize");
 
-// Init the DB Connection
-const sequelize = new Sequelize(process.env.DATABASE_URL);
+// Some copypasta to make sequilize work with Heroku Postgres
+// https://bootcamp.rocketacademy.co/4-backend-structure/4.1-orm-sequelize/4.1.10-deploy-sequelize-app-to-heroku
+const url = require("url");
+let sequelize;
+console.log(
+  "Node env: ",
+  process.env.NODE_ENV,
+  process.env.NODE_ENV === "production"
+);
+if (process.env.NODE_ENV === "production") {
+  const config = {
+    use_env_variable: "DATABASE_URL",
+    dialect: "postgres",
+    protocol: "postgres",
+    dialectOptions: {
+      ssl: {
+        require: true,
+        rejectUnauthorized: false,
+      },
+    },
+  };
+  const { DATABASE_URL } = process.env;
+  const dbUrl = url.parse(DATABASE_URL);
+  const username = dbUrl.auth.substr(0, dbUrl.auth.indexOf(":"));
+  const password = dbUrl.auth.substr(
+    dbUrl.auth.indexOf(":") + 1,
+    dbUrl.auth.length
+  );
+  const dbName = dbUrl.path.slice(1);
+  const host = dbUrl.hostname;
+  const { port } = dbUrl;
+  config.host = host;
+  config.port = port;
+  sequelize = new Sequelize(dbName, username, password, config);
+} else {
+  sequelize = new Sequelize(process.env.DATABASE_URL);
+}
 
-// Load User model
 require("./User.model")(sequelize);
 
 module.exports = sequelize;
